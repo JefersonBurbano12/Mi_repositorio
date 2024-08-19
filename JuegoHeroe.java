965a80930ea3ae211cc8d70f5f2ccd77527f3e66
Jeferson <japastas@unicauca.edu.co>
2024-08-19 13:33:33 -0500

    Se agrego una nueva linea

--- JuegoHeroe.java
+++ JuegoHeroe.java
@@ -15,6 +15,8 @@ public class JuegoHeroe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        int nuevo = 12; 
         // TODO code application logic here
     }
     
